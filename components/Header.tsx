a59f65f2df451019b1677ca4ab16b6c260276816
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-07-23 23:28:36 +0330

    design header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image"
 import Logo from "@/public/logoHeader.png"
 import NightLogo from "@/public/nightLogo.svg"
+import Link from "next/link"
 export const Header = () => {
   return (
     <>
       <div className="flex justify-center items-center gap-40">
-        <Image className="w-36 h-28" src={Logo} alt="header_logo" />
-        <div className="list list-none flex items-center gap-16 text-lg">
-          <li> Services </li>
-          <li> Portfolios </li>
-          <li> Experience </li>
-          <li> Blog </li>
+        <Link href={'/'}>
+          <Image className="w-36 h-28" src={Logo} alt="header_logo" />
+        </Link>
+        <div className="list list-none flex items-center gap-20 text-lg">
+          <li className="cursor-pointer"> <Link href={'/'}>Services
+          </Link> </li>
+          <li className="cursor-pointer"> <Link href={'/'}>Portfolios</Link> </li>
+          <li className="cursor-pointer"> <Link href={'/'}>Experience
+          </Link> </li>
+          <li className="cursor-pointer"> <Link href={'/'}>Blog</Link> </li>
         </div>
         <div className="options flex items-center gap-8">
-          <Image className="w-16 h-16" src={NightLogo} alt='Night mode' />
-          <button className="text-white px-6 py-2.5 border-2 rounded-xl"> Lebenslauf </button>
+          <Image className="w-16 h-16 cursor-pointer" src={NightLogo} alt='Night mode' />
+          <button className="text-white px-6 py-2.5 border-2 rounded-xl hover:bg-white hover:text-black delay-100 cursor-pointer"> Lebenslauf </button>
         </div>
       </div>
     </>
