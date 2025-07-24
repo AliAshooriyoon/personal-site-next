de30349e3d702271b873ecd7f60e88e5d91cdf3f
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-07-24 21:25:16 +0330

    structur for projects box 2

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,10 @@
+import "./projects.css"
 const Projects = () => {
   return (
     <>
       <div className="">
-        <h3 className="projects_title text-3xl text-center pt-32">Meine Projekte :</h3>
-        <div className="projects_box">
+        <h3 className="projects_title text-3xl text-left pl-22 pt-32">Meine Projekte </h3>
+        <div className="krojects_box flex gap-18 justify-center items-center p-12 ">
           <div className="project_box"></div>
           <div className="project_box"></div>
           <div className="project_box"></div>
