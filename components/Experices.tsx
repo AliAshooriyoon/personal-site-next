d35798ae97e4b14d7fcc239a93cc4c444c51603f
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-07-25 16:03:59 +0330

    design history box

--- components/Experices.tsx
+++ components/Experices.tsx
@@ -1,25 +1,32 @@
+"use client"
+import { useState } from "react"
 import "./Experices.css"
+import { expericesData } from "@/expericesData"
 const Experices = () => {
+  console.log(expericesData)
+  const [selectedEx, setSelectedEx] = useState(1);
   return (
     <>
       <div className="bg-[rgb(21,19,19,56%)] py-24 px-12">
         <p className="title_experices text-4xl">Berufliche Erfahrungen</p>
         <div className="main_experices flex items-center flex-row justify-start">
           <div className="names_experices list-none flex  flex-col gap-12 p-8 w-80">
-            <li className="name_experices border-2 p-2 rounded-lg cursor-pointer">Code Amooz
-              {/* <span> */}
-              {/*   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-6"> */}
-              {/*     <path fillRule="evenodd" d="M2 8c0 .414.336.75.75.75h8.69l-1.22 1.22a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06l-2.5-2.5a.75.75 0 1 0-1.06 1.06l1.22 1.22H2.75A.75.75 0 0 0 2 8Z" clipRule="evenodd" /> */}
-              {/*   </svg> */}
-              {/**/}
-              {/* </span> */}
-            </li>
-            <li className="selected name_experices border-2 p-2 rounded-lg flex items-center justify-around cursor-pointer">Sepahanyadaki
-              <span>
+            <li onClick={() => setSelectedEx(1)} className={` ${selectedEx == 1 ? "selected flex items-center justify-around" : ""}
+        name_experices  border-2 p-2 rounded-lg cursor-pointer`}>Code Amooz
+              {selectedEx == 1 && <span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-6">
                   <path fillRule="evenodd" d="M2 8c0 .414.336.75.75.75h8.69l-1.22 1.22a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06l-2.5-2.5a.75.75 0 1 0-1.06 1.06l1.22 1.22H2.75A.75.75 0 0 0 2 8Z" clipRule="evenodd" />
                 </svg>
-              </span>
+              </span>}
+            </li>
+            <li onClick={() => setSelectedEx(2)} className={` ${selectedEx == 2 ? "selected flex items-center justify-around" : ""}
+              name_experices border-2 p-2 rounded-lg
+               cursor-pointer `}>Sepahanyadaki
+              {selectedEx == 2 && <span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-6">
+                  <path fillRule="evenodd" d="M2 8c0 .414.336.75.75.75h8.69l-1.22 1.22a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06l-2.5-2.5a.75.75 0 1 0-1.06 1.06l1.22 1.22H2.75A.75.75 0 0 0 2 8Z" clipRule="evenodd" />
+                </svg>
+              </span>}
             </li>
           </div>
           <div className="details_experices pl-28 flex flex-col gap-2 pt-12">
