c5ac936c6d93b5e75be1e43fa58411aa65ba6e36
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-07-25 09:50:25 +0330

    project options

--- components/ProjectBox.tsx
+++ components/ProjectBox.tsx
@@ -1,16 +1,16 @@
 import "./ProjectBox.css";
 
-const ProjectBox = ({ imageAdresse = "fallback" }) => {
+const ProjectBox = ({ imageAdresse = "fallback", title }) => {
   return (
-    <div className="bg-none relative project_box w-80 h-80">
+    <div className="bg-none relative project_box w-80 h-80 cursor-pointer">
       <div
         className="relative w-full h-full rounded-2xl bg-center  inset-0 bg-no-repeat bg-cover opacity-20"
         style={{ backgroundImage: `url('/${imageAdresse}.jpg')` }}
       ></div>
-      <div className="absolute z-10 bottom-0 left-0 p-2 rounded-b-2xl w-full bg-stone-300 text-black">
+      <div className="absolute z-10 bottom-0 left-0 p-4 rounded-b-2xl w-full bg-stone-300 text-black">
         <div className="flex flex-col items-start gap-2">
-          <span className="text-black project_title">ToDo list project</span>
-          <span className="text-black project_lang border-2 p-1 rounded-xl text-center">React.js</span>
+          <span className="text-black project_title">{title}</span>
+          <span className="text-black project_lang border-2 border-stone-500 py-1 px-3 rounded-xl text-[0.7rem] text-center">React.js</span>
 
         </div>
       </div>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,9 +6,9 @@ const Projects = () => {
       <div className="">
         <h3 className="projects_title text-3xl text-left pl-22 pt-32">Meine Projekte</h3>
         <div className="krojects_box flex gap-18 justify-center items-center p-12">
-          <ProjectBox imageAdresse={'3d-illustration-hand-putting-tick-paper'} />
-          <ProjectBox imageAdresse={'lorem'} />
-          <ProjectBox imageAdresse={"shopLogo"} />
+          <ProjectBox title={'ToDo list project'} imageAdresse={'3d-illustration-hand-putting-tick-paper'} />
+          <ProjectBox title={'Lorem ipsum generator'} imageAdresse={'lorem'} />
+          <ProjectBox title={'Shop project'} imageAdresse={"shopLogo"} />
         </div>
       </div>
     </>
