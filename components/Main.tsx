1e0abed9edade56d2823ddc33de6f3ba51b555f9
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-07-31 21:34:52 +0330

    responsive main

--- components/Main.tsx
+++ components/Main.tsx
@@ -9,10 +9,10 @@ export const Main = ({ dict }: MainProps) => {
 
   return (
     <>
-      <div className="flex pt-20 px-36 pb-36 justify-around">
+      <div className="flex pt-20 md:px-36 md:pb-36 gap-16 w-full justify-around max-md:flex-col-reverse max-md:gap-12">
         <div className="main_text">
-          <h1 className="text-4xl mb-6">Ali <span className="text-[#023E6F]">Ashouriyoun</span> </h1>
-          <h2 className="max-w-[32rem] leading-8">
+          <h1 className="text-4xl mb-6 flex gap-4 flex-wrap pl-4"> <span className="indent-2">Ali</span> <span className="text-[#023E6F]">Ashouriyoun</span> </h1>
+          <h2 className="max-w-[32rem] leading-8 max-sm:px-4 py-2 lg:text-xl max-sm:text-md">
             {dict.details}
             {/* Ich bin Ali, 18 Jahre alt und arbeite als Frontend-Entwickler. */}
             {/* Ich interessiere mich nicht nur für Frontend, sondern auch */}
@@ -22,9 +22,9 @@ export const Main = ({ dict }: MainProps) => {
           </h2>
           <div className="logos_networking"></div>
         </div>
-        <div className="picture_box">
-          <div className="w-72 h-96 bg-[#023E6F] rounded-2xl -rotate-14">
-            <div className="relative w-72 h-96 bg-stone-400 rounded-2xl rotate-14 overflow-hidden">
+        <div className="picture_box max-sm:w-full  max-sm:flex justify-center items-center">
+          <div className="w-72 max-[320px]:w-52 max-sm:w-60 h-96 mx-auto  bg-[#023E6F] rounded-2xl -rotate-14">
+            <div className="relative max-[320px]:w-52 max-sm:right-1/2 max-sm:left-1/2 max-sm:-translate-x-1/2 max-sm:w-[99%]  w-72 h-96 bg-stone-400 rounded-2xl rotate-14 overflow-hidden">
               <Image className="absolute -bottom-8 -left-5 h-[90%] w-[80%] rounded-2xl opacity-85 " src={myPic} alt="myPic" />
             </div>
           </div>
