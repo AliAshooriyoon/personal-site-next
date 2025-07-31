dd912b67ad606758f127a09817d49810c3d3e533
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-07-31 21:55:43 +0330

    responsive services

--- components/Services.tsx
+++ components/Services.tsx
@@ -15,7 +15,7 @@ const Services = ({ dict }: ServiceProps) => {
     <>
       <Element name='services' className="w-full bg-[rgb(21,19,19,56%)] text-center py-28">
         <h3 className='text-4xl pb-8'>{dict.what}</h3>
-        <div className="services_boxes flex gap-20 justify-center p-4">
+        <div className="services_boxes max-md:flex-col max-md:justify-center max-md:items-center max-md:w-full flex gap-20 justify-center md:p-4">
           <ServicesBox dict={dict} title={dict.title1} />
           <ServicesBox dict={dict} title={dict.title2} />
           <ServicesBox dict={dict} title={dict.title3} />
