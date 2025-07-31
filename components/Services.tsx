7d10e6c9a8007301cab178d21b14c8154b3ac281
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-07-31 23:59:17 +0330

    change title size in services

--- components/Services.tsx
+++ components/Services.tsx
@@ -14,7 +14,7 @@ const Services = ({ dict }: ServiceProps) => {
   return (
     <>
       <Element name='services' className="w-full bg-[rgb(21,19,19,56%)] text-center py-28">
-        <h3 className='text-4xl pb-8'>{dict.what}</h3>
+        <h3 className='text-4xl pb-8 max-sm:text-3xl'>{dict.what}</h3>
         <div className="services_boxes max-md:flex-col max-md:justify-center max-md:items-center max-md:w-full flex gap-20 justify-center md:p-4">
           <ServicesBox dict={dict} title={dict.title1} />
           <ServicesBox dict={dict} title={dict.title2} />
